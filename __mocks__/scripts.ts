6025191aa7fbbdeeac233eba33fe59feb916a27d
Scott Sullivan <ssulivan11@gmail.com>
2023-07-02 11:33:25 -0400

    Better colours and icons

--- __mocks__/scripts.ts
+++ __mocks__/scripts.ts
@@ -9,7 +9,7 @@ shell.exec(buildType, { silent: true }, (code: Number, stdout: string) => {
   if (buildTypeArg === 'valid') {
     const validMsgCount = stdout.split('success').length;
     const warnMsgCount = stdout.split('warning').length;
-    // ensure we are successful in 5 and warned in 3 checks
+    // ensure we are successful and warning the needed
     console.warn(`code: ${code}, validMsgCount: ${validMsgCount}, warnMsgCount: ${warnMsgCount}, `);
     if (code !== 0 || validMsgCount !== 5 || warnMsgCount !== 3) {
       console.log(`${emojiMsg.get('x')} - ${colors.red('Invalid exit code or size message returned')}\n\n`);
@@ -21,7 +21,7 @@ shell.exec(buildType, { silent: true }, (code: Number, stdout: string) => {
 
   const invalidMsgCount = stdout.split('failure').length;
   const warnMsgCount = stdout.split('warning').length;
-  // ensure we are failing all 5
+  // ensure we are failing and warning the needed
   console.warn(`code: ${code}, invalidMsgCount: ${invalidMsgCount}, warnMsgCount: ${warnMsgCount}, `);
   if (code !== 1 || invalidMsgCount !== 6 || warnMsgCount !== 2) {
     console.log(`${emojiMsg.get('x')} - ${colors.red('Invalid exit code or size message returned')}\n\n`);
