74878479522df5fc561637fca3eb0a83dbf41022
Scott Sullivan <ssulivan11@gmail.com>
2023-07-02 11:02:13 -0400

    mock scripts testing in github actions

--- __mocks__/scripts.ts
+++ __mocks__/scripts.ts
@@ -11,7 +11,7 @@ shell.exec(buildType, { silent: true }, (code: Number, stdout: string) => {
     const warnMsgCount = stdout.split('warning').length;
     // ensure we are successful in 5 and warned in 3 checks
     console.warn(`code: ${code}, validMsgCount: ${validMsgCount}, warnMsgCount: ${warnMsgCount}, `);
-    if (code !== 0 || validMsgCount !== 5 || warnMsgCount !== 3) {
+    if (code !== 0 || validMsgCount !== 6 || warnMsgCount !== 2) {
       console.log(`${emojiMsg.get('x')} - ${colors.red('Invalid exit code or size message returned')}\n\n`);
       return shell.exit(1);
     }
