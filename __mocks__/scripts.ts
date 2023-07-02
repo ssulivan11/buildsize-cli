a2ac27051389894e38528dd51d943b34dd298376
Scott Sullivan <ssulivan11@gmail.com>
2023-07-02 10:56:01 -0400

    mock scripts testing in github actions

--- __mocks__/scripts.ts
+++ __mocks__/scripts.ts
@@ -10,6 +10,7 @@ shell.exec(buildType, { silent: true }, (code: Number, stdout: string) => {
     const validMsgCount = stdout.split('success').length;
     const warnMsgCount = stdout.split('warning').length;
     // ensure we are successful in 5 and warned in 3 checks
+    console.warn(`code: ${code}, validMsgCount: ${validMsgCount}, warnMsgCount: ${warnMsgCount}, `);
     if (code !== 0 || validMsgCount !== 5 || warnMsgCount !== 3) {
       console.log(`${emojiMsg.get('x')} - ${colors.red('Invalid exit code or size message returned')}\n\n`);
       return shell.exit(1);
@@ -21,6 +22,7 @@ shell.exec(buildType, { silent: true }, (code: Number, stdout: string) => {
   const invalidMsgCount = stdout.split('failure').length;
   const warnMsgCount = stdout.split('warning').length;
   // ensure we are failing all 5
+  console.warn(`code: ${code}, invalidMsgCount: ${invalidMsgCount}, warnMsgCount: ${warnMsgCount}, `);
   if (code !== 1 || invalidMsgCount !== 6 || warnMsgCount !== 2) {
     console.log(`${emojiMsg.get('x')} - ${colors.red('Invalid exit code or size message returned')}\n\n`);
     return shell.exit(1);
