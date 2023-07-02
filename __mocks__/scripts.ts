17a0b528653ce28acc974ce8195733e802d8fd17
Scott Sullivan <ssulivan11@gmail.com>
2023-07-02 11:38:20 -0400

    Proper valid count

--- __mocks__/scripts.ts
+++ __mocks__/scripts.ts
@@ -10,8 +10,8 @@ shell.exec(buildType, { silent: true }, (code: Number, stdout: string) => {
     const validMsgCount = stdout.split('success').length;
     const warnMsgCount = stdout.split('warning').length;
     // ensure we are successful and warning the needed
-    console.warn(`code: ${code}, validMsgCount: ${validMsgCount}, warnMsgCount: ${warnMsgCount}, `);
-    if (code !== 0 || validMsgCount !== 5 || warnMsgCount !== 3) {
+    console.warn(`${stdout}\n\n - code: ${code}\n - validMsgCount: ${validMsgCount}\n - warnMsgCount: ${warnMsgCount}, `);
+    if (code !== 0 || validMsgCount !== 6 || warnMsgCount !== 2) {
       console.log(`${emojiMsg.get('x')} - ${colors.red('Invalid exit code or size message returned')}\n\n`);
       return shell.exit(1);
     }
@@ -22,7 +22,7 @@ shell.exec(buildType, { silent: true }, (code: Number, stdout: string) => {
   const invalidMsgCount = stdout.split('failure').length;
   const warnMsgCount = stdout.split('warning').length;
   // ensure we are failing and warning the needed
-  console.warn(`code: ${code}, invalidMsgCount: ${invalidMsgCount}, warnMsgCount: ${warnMsgCount}, `);
+  console.warn(`code: ${code}\n - invalidMsgCount: ${invalidMsgCount}\n - warnMsgCount: ${warnMsgCount}, `);
   if (code !== 1 || invalidMsgCount !== 6 || warnMsgCount !== 2) {
     console.log(`${emojiMsg.get('x')} - ${colors.red('Invalid exit code or size message returned')}\n\n`);
     return shell.exit(1);
