dd6cf5eb28591d8d7a46c58c5f1de2d15b25c546
Scott Sullivan <ssulivan11@gmail.com>
2023-07-02 11:40:31 -0400

    Format fix

--- __mocks__/scripts.ts
+++ __mocks__/scripts.ts
@@ -10,7 +10,9 @@ shell.exec(buildType, { silent: true }, (code: Number, stdout: string) => {
     const validMsgCount = stdout.split('success').length;
     const warnMsgCount = stdout.split('warning').length;
     // ensure we are successful and warning the needed
-    console.warn(`${stdout}\n\n - code: ${code}\n - validMsgCount: ${validMsgCount}\n - warnMsgCount: ${warnMsgCount}, `);
+    console.warn(
+      `${stdout}\n\n - code: ${code}\n - validMsgCount: ${validMsgCount}\n - warnMsgCount: ${warnMsgCount}, `
+    );
     if (code !== 0 || validMsgCount !== 6 || warnMsgCount !== 2) {
       console.log(`${emojiMsg.get('x')} - ${colors.red('Invalid exit code or size message returned')}\n\n`);
       return shell.exit(1);
