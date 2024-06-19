8efd05277c57fbda97e2fd2173592c67abd6c636
Scott Sullivan <ssulivan11@gmail.com>
2024-06-19 10:24:16 -0400

    update unit tests

--- src/__tests__/sizes.spec.ts
+++ src/__tests__/sizes.spec.ts
@@ -5,17 +5,17 @@ describe('size compression', () => {
   const mock5kbFileSize = '__mocks__/5kb.gzip.mock.ts';
   test('should return valid gzip size', () => {
     const size = sizes(fs.readFileSync(mock5kbFileSize, 'utf8'), 'gzip');
-    expect(size).toBe(5139);
+    expect(size).toBe(5125);
   });
 
   test('should return valid brotli size', () => {
     const size = sizes(fs.readFileSync(mock5kbFileSize, 'utf8'), 'brotli');
-    expect(size).toBe(102);
+    expect(size).toBe(95);
   });
 
   test('should return valid raw size', () => {
     const size = sizes(fs.readFileSync(mock5kbFileSize, 'utf8'));
-    expect(size).toBe(1129944);
+    expect(size).toBe(1129773);
   });
 });
 
